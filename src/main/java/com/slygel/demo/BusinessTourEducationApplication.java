050ab7efc8feb852999a125acf367d30c9555eb6
Nguyễn Tài Tuệ <nttue03@gmail.com>
2024-03-07 22:00:30 +0700

    Khoi tao project

---
+++ src/main/java/com/slygel/demo/BusinessTourEducationApplication.java
@@ -0,0 +1,16 @@
+package com.slygel.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class BusinessTourEducationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BusinessTourEducationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/slygel/demo/BusinessTourEducationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.slygel.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BusinessTourEducationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
